0c9eb421a0aaf5682b2aed4ca389b126187dec5f
FABIAN ESTEBAN LOPEZ ARIAS <fabian_esteban.lopez@uao.edu.co>
2024-08-29 18:59:58 -0500

    df

--- src/Views/Landing.jsx
+++ src/Views/Landing.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import style from "../Styles/Landing.module.css";
-//import Footer from "../components/Footer";
-//import video from "../img/trailer.mp4"
+import video from "../img/trailer.mp4";
 
 export default function Landing() {
   return (
-    <div>     
-      <div className={style.div}  >
-        <h1 className={style.title}>¡ 🥣Welcome🥣 !</h1>
-        <h1 className={style.subTitle}>🍳 Grupal 🥗 Proyect 🥗 Food 🍳</h1>
+    <div>
+      <video src={video} autoPlay muted loop className={style.video}></video>
+      <div className={style.div}>
+        <h1 className={style.title}>¡ 🥣Bienvenidos 🥣 !</h1>
+        <h1 className={style.subTitle}>🍳 Galeria 🥗 Alameda 🥗 Digital 🍳</h1>
         <Link to="/home">
-          <button className={style.botonLanding}>Enter</button>
+          <button className={style.botonLanding}>Ingresar</button>
         </Link>
-        
-        </div>
+      </div>
     </div>
   );
 }
